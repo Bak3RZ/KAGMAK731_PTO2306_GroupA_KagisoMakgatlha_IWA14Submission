af8204d228d088e9bb66dbd4c9957ac7f0c26515
Kagiso <101631291+Bak3RZ@users.noreply.github.com>
2023-11-09 04:23:49 +0200

    IWA 14 Challenge 01
    Challenge 01

---
+++ script.js
@@ -0,0 +1,17 @@
+firstName = 'John';
+age = 35;
+hobby = 'Coding';
+
+const logTwice = (parameter) => { 
+  console.log(parameter) 
+  console.log(parameter)
+}
+
+// The function logTwice takes a parameter, which is a string. This string is passed to console.log two times. As a result, the string is logged in the console twice.
+
+const logHobby = () => { // renamed hobby to logHobby
+  logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
+}
+
+logHobby()
+// "hobby" has a specific meaning that doesn't fit with the context of a variable used for logging. The name "logHobby" more accurately represents the purpose of the variable.
